1cfa3b7b1571bb15ecac04d095cf9993675784aa
precarbon <129496922+precarbon@users.noreply.github.com>
2023-04-01 19:00:31 -0400

    Initialize repro-object-is-not-a-function

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import IndexPage from 'components/IndexPage'
+import { getAllPosts, getSettings } from 'lib/sanity.client'
+
+// FIXME: https://github.com/sanity-io/nextjs-blog-cms-sanity-v3/issues/95
+
+export default async function IndexRoute() {
+  // Fetch queries in parallel
+  const [settings, posts] = await Promise.all([getSettings(), getAllPosts()])
+
+  return <IndexPage posts={posts} settings={settings} />
+}
---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,38 @@
+import PostPage from 'components/PostPage'
+import {
+  getAllPostsSlugs,
+  getPostAndMoreStories,
+  getSettings,
+} from 'lib/sanity.client'
+
+export async function generateStaticParams() {
+  return await getAllPostsSlugs()
+}
+
+export default async function SlugRoute({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  // Start fetching settings early, so it runs in parallel with the post query
+  const settings = getSettings()
+
+  /*
+  import { PreviewSuspense } from 'components/PreviewSuspense'
+  import { previewData } from 'next/headers'
+  if (previewData()) {
+    const token = previewData().token || null
+    const data = getPostAndMoreStories(params.slug, token)
+    return (
+      <PreviewSuspense
+        fallback={<PostPage loading preview data={await data} settings={await settings} />}
+      >
+        <PreviewPostPage token={token} slug={params.slug} />
+      </PreviewSuspense>
+    )
+  }
+  // */
+
+  const data = getPostAndMoreStories(params.slug)
+  return <PostPage data={await data} settings={await settings} />
+}
